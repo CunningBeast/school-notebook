f6c2eea671d4d6379970fb7c48a1ad8c27e7d8ce
TheCunningBeast <the.cunning.beast@gmail.com>
2025-02-11 09:44:22 +0000

    chore: refactor next auth

--- src/entities/session/auth.ts
+++ src/entities/session/auth.ts
@@ -1,9 +1,4 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { dbClient } from "@/shared/lib/db";
+import { nextAuthConfig } from "./next-auth-config";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(dbClient),
-  providers: [GithubProvider],
-});
+export const { handlers, signIn, signOut, auth } = NextAuth(nextAuthConfig);
