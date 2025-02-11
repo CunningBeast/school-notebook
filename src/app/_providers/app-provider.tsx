7b974f6198ef38b0a21ce8888ad5e1c945550295
TheCunningBeast <the.cunning.beast@gmail.com>
2025-02-11 11:12:34 +0000

    feat: add auth

--- src/app/_providers/app-provider.tsx
+++ src/app/_providers/app-provider.tsx
@@ -2,11 +2,16 @@
 
 import { ComposeChildren } from "@/shared/lib/react";
 import { ThemeProvider } from "../../entities/theme/theme-provider";
+import { AppSessionProvider } from "@/entities/session/app-session-provider";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/shared/api/query-client";
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
   return (
     <ComposeChildren>
       <ThemeProvider />
+      <AppSessionProvider />
+      <QueryClientProvider client={queryClient} />
       {children}
     </ComposeChildren>
   );
