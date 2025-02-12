17e32fac4786c0287da2638a0dbb05918fac58f9
TheCunningBeast <the.cunning.beast@gmail.com>
2025-02-12 08:31:29 +0000

    feat: add email auth

--- src/entities/session/next-auth-config.ts
+++ src/entities/session/next-auth-config.ts
@@ -1,9 +1,24 @@
 import { NextAuthConfig } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import EmailProvider from "next-auth/providers/email";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { dbClient } from "@/shared/lib/db";
+import { privateConfig } from "@/shared/config/private";
 
 export const nextAuthConfig: NextAuthConfig = {
   adapter: PrismaAdapter(dbClient),
-  providers: [GithubProvider],
+  providers: [
+    EmailProvider({
+      server: {
+        host: privateConfig.EMAIL_SERVER_HOST,
+        port: privateConfig.EMAIL_SERVER_PORT,
+        auth: {
+          user: privateConfig.EMAIL_SERVER_USER,
+          pass: privateConfig.EMAIL_SERVER_PASSWORD,
+        },
+        from: privateConfig.EMAIL_FROM,
+      },
+    }),
+    GithubProvider,
+  ],
 };
